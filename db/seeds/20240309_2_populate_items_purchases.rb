df77da215b6efe9b894ddf0dd9b6191d3ad42a38
Manuel Alejandro <malabaut@gmail.com>
2024-03-10 07:39:25 -0300

    Fix: remove debug helpers

--- db/seeds/20240309_2_populate_items_purchases.rb
+++ db/seeds/20240309_2_populate_items_purchases.rb
@@ -14,18 +14,12 @@ Sequel.seed(:development, :test) do
                                     WHERE purchases.id = items.purchase_id
     )').all
 
-    p purchases_list.count
-
     items_list = db.fetch('SELECT id FROM items WHERE purchase_id IS NULL').all
 
-    i = 1
     purchases_list.each do |purchase|
-      p items_list
-      p item_id = items_list.shuffle!.pop[:id]
+      item_id = items_list.shuffle!.pop[:id]
       ds = db['UPDATE items SET purchase_id = ? WHERE id = ?', purchase[:id], item_id]
       ds.update
-      p i
-      i += 1
     end
   end
 end
