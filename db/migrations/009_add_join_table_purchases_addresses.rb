2186c38077baf619eba3f585d1593cd332114586
Manuel Alejandro <malabaut@gmail.com>
2024-03-10 00:47:27 -0300

    Fix: change the name of addresses_purchases table

--- db/migrations/009_add_join_table_purchases_addresses.rb
+++ db/migrations/009_add_join_table_purchases_addresses.rb
@@ -4,6 +4,6 @@ Sequel.migration do
   transaction
 
   up do
-    create_join_table(purchase_id: :purchases, address_id: :addressess)
+    create_join_table(purchase_id: :purchases, address_id: :addresses)
   end
 end
